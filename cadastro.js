49cc1da74eab1a02856da6f17ba554b45c925d5b
RamonRauber <162449700+RamonRauber@users.noreply.github.com>
2025-09-30 11:31:26 -0300

    Add files via upload

--- cadastro.js
+++ cadastro.js
@@ -1,3 +1,4 @@
+const formCliente = document.getElementById("form-cliente")
 formCliente.addEventListener("submit", async (event) => {
     event.preventDefault();
 
@@ -30,9 +31,9 @@ function pegarDadosCliente(){
     cliente.cpf = document.getElementById("cpf").value
     cliente.email = document.getElementById("email").value
     cliente.telefone = document.getElementById("telefone").value
-    cliente.cep = document.getElementById("cep").value
+    cliente.cep = document.getElementById("cep").value.replace(/\D/g, '');
     cliente.rua = document.getElementById("rua").value
-    cliente.n_casa = document.getElementById("numero").value
+    cliente.n_casa = document.getElementById("n_casa").value
     cliente.bairro = document.getElementById("bairro").value
     cliente.cidade = document.getElementById("cidade").value
     cliente.uf = document.getElementById("uf").value
